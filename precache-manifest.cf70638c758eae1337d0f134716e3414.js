64a276406d3ffd133d1005048bf3a519c750abbf
philipstubbs13 <philipstubbs13@gmail.com>
2020-02-23 20:07:24 -0600

    Updates

---
+++ precache-manifest.cf70638c758eae1337d0f134716e3414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9707916134a6632de6bf8711a3270bd7",
+    "url": "/youtube-trending-analysis/index.html"
+  },
+  {
+    "revision": "094f7c80e8a365bf20bc",
+    "url": "/youtube-trending-analysis/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5525b4f9d05c94e8e017",
+    "url": "/youtube-trending-analysis/static/js/2.10eb71ec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/youtube-trending-analysis/static/js/2.10eb71ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094f7c80e8a365bf20bc",
+    "url": "/youtube-trending-analysis/static/js/main.7b79dca8.chunk.js"
+  },
+  {
+    "revision": "1215b63dd9742b5be53d",
+    "url": "/youtube-trending-analysis/static/js/runtime-main.c62c1f79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/youtube-trending-analysis/static/media/logo.5d5d9eef.svg"
+  }
+]);
